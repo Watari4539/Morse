5721b87b589da9b70ab9326dcdb4d49352943e7f
Adrian <ruizjoseadrian74@email.com>
2026-04-30 00:06:56 -0600

    Initial Commit

---
+++ Morse/MorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseApp.swift
+//  Morse
+//
+//  Created by Adrián Nieto on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
